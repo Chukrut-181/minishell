d87e930c6e8e461d46ef6d2d4c23d4fdcbeebad3
eandres <eandres@student.42urduliz.com>
2024-11-05 13:04:24 +0100

    update

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: eandres <eandres@student.42urdudilz.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:19:09 by eandres           #+#    #+#             */
-/*   Updated: 2024/11/02 13:24:26 by eandres          ###   ########.fr       */
+/*   Updated: 2024/11/05 12:35:34 by eandres          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <limits.h>
+#include <signal.h>
 #include "../lib/libft/libft.h"
+
+#define RED			"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define GREENB		"\033[32;1m"
+# define YELLOW		"\033[0;33m"
+# define BLUE 		"\033[0;34m"
+# define BLUEB 		"\033[34;1m"
+# define PURPLE		"\033[0;35m"
+# define PURPLEB	"\033[35;1m"
+# define CYAN		"\033[0;36m"
+# define BOLD		"\033[0;1m"
+# define X			"\033[0;0m"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,28 +6,32 @@
 /*   By: eandres <eandres@student.42urdudilz.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:25:53 by eandres           #+#    #+#             */
-/*   Updated: 2024/11/02 20:29:30 by eandres          ###   ########.fr       */
+/*   Updated: 2024/11/05 12:45:29 by eandres          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 char *get_name(char **env)
 {
 	char *user = NULL;
     char *pwd = NULL;
+	int i = 0;
 
 	if (!env)
 		return (NULL);
-	while (*env)
+	while (env[i])
 	{
-		if (ft_strncmp("USER=", *env, 5) == 0)
-			user = *env + 5;
-		else if (ft_strncmp("PWD=", *env, 4) == 0)
-			pwd = *env + 4;
-		env++;
+		if (ft_strncmp("USER=", env[i], 5) == 0)
+			user = ft_strjoin(YELLOW, env[i] + 5);
+		else if (ft_strncmp("PWD=", env[i], 4) == 0)
+			pwd = ft_strjoin(BLUE, env[i] + 4);
+		i++;
 	}
-	char *prompt = ft_strjoin(user, "@minishell ");
+	pwd = ft_strjoin(pwd, BLUE" $ "X);
+	char *prompt = ft_strjoin(user, YELLOW"@minishell "X);
 	char *result = ft_strjoin(prompt, pwd);
 	free(prompt);
 	return (result);
@@ -45,7 +49,12 @@ int main(int argc, char **argv, char **env)
 	{
 		name = get_name(env);
 		line = readline(name);
+		add_history(line);
+		rl_on_new_line();
 	}
+	printf("el historial es: %d\n", history_length);
+	rl_clear_history();
+	printf("el historial es: %d\n", history_length);
 	free(line);
 	return (0);
 }
