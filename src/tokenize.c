fc1e4baa5b4ecef8362cb3ba70db0a6953f92212
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2024-11-05 11:19:23 +0100

    Norme.

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:23:07 by igchurru          #+#    #+#             */
-/*   Updated: 2024/11/05 10:30:19 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/11/05 10:58:02 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,68 +17,80 @@
 // Set up indices for iterating over 'line' and the buffer
 // Variable to track if we are inside a quote. This is set to '"' or '\'' if inside quotes, otherwise '\0'
 // Main loop: Iterate through each character of the input line
-    // Case 1: Handle spaces outside of quotes (indicates the end of a token)
-            // Check if there is any text in the buffer to create a token
-t_token *tokenize(char *line)
+	// Case 1: Handle spaces outside of quotes (indicates the end of a token)
+			// Check if there is any text in the buffer to create a token
+			// Null-terminate the buffer
+			// Add the buffer as a token to the list
+			// Reset buffer index for the next token
+	// Case 2: Handle quote characters (either double " or single ')
+			// Check if we're already inside a quote
+			// Start of a quote, store the quote character
+			// End of the current quote, reset quote to '\0'
+			// Add character to buffer if inside quotes
+	// Case 3: Handle operators (|, >, <) outside of quotes
+			// If there are characters in the buffer, finalize and add the current token
+			// Add the operator itself as a separate token
+			// Null-terminate the operator string
+	// Case 4: Normal character, add to the current buffer
+	// Move to the next character in the line
+// After the loop, check if there’s any remaining token in the buffer
+	// Null-terminate the buffer
+	// Add the final token to the list
+// Return the head of the linked list containing all tokens
+t_token	*tokenize(char *line)
 {
-    t_token *head = NULL;
-    
-    char buffer[1024];
-    int i = 0, buf_index = 0;
+	t_token	*head;
+	char	buffer[1024];
+	int		i;
+	int		buf_index;
+	char	quote;
 
-    char quote = '\0';  // 
-
-    while (line[i] != '\0') 
+	head = NULL;
+	i = 0;
+	buf_index = 0;
+	quote = '\0';
+	while (line[i] != '\0')
 	{
-        if (line[i] == ' ' && !quote) 
-		{ 
-            if (buf_index > 0) 
-			{
-                buffer[buf_index] = '\0';  // Null-terminate the buffer
-                add_token(&head, buffer);  // Add the buffer as a token to the list
-                buf_index = 0;             // Reset buffer index for the next token
-            }
-        }
-
-        // Case 2: Handle quote characters (either double " or single ')
-        else if (line[i] == '"' || line[i] == '\'')
-		{  
-            // Check if we're already inside a quote
-            if (!quote)
-                quote = line[i];  // Start of a quote, store the quote character
-            else if (quote == line[i])
-                quote = '\0';  // End of the current quote, reset quote to '\0'
-            else
-                buffer[buf_index++] = line[i];  // Add character to buffer if inside quotes
-        }
-
-        // Case 3: Handle operators (|, >, <) outside of quotes
-        else if (ft_strchr("|<>", line[i]) && !quote)
-		{ 
-            // If there are characters in the buffer, finalize and add the current token
-            if (buf_index > 0) { 
-                buffer[buf_index] = '\0';
-                add_token(&head, buffer);
-                buf_index = 0;
-            }
-            // Add the operator itself as a separate token
-            buffer[0] = line[i];
-            buffer[1] = '\0';  // Null-terminate the operator string
-            add_token(&head, buffer);
-        }
-        // Case 4: Normal character, add to the current buffer
-        else
+		if (line[i] == ' ' && !quote)
 		{
-            buffer[buf_index++] = line[i];
-        }
-        i++;  // Move to the next character in the line
-    }
-    // After the loop, check if there’s any remaining token in the buffer
-    if (buf_index > 0) {
-        buffer[buf_index] = '\0';  // Null-terminate the buffer
-        add_token(&head, buffer);  // Add the final token to the list
-    }
-    return (head);  // Return the head of the linked list containing all tokens
+			if (buf_index > 0)
+			{
+				buffer[buf_index] = '\0';
+				add_token(&head, buffer);
+				buf_index = 0;
+			}
+		}
+		else if (line[i] == '"' || line[i] == '\'')
+		{
+			if (!quote)
+				quote = line[i];
+			else if (quote == line[i])
+				quote = '\0';
+			else
+				buffer[buf_index++] = line[i];
+		}
+		else if (ft_strchr("|<>", line[i]) && !quote)
+		{
+			if (buf_index > 0)
+			{
+				buffer[buf_index] = '\0';
+				add_token(&head, buffer);
+				buf_index = 0;
+			}
+			buffer[0] = line[i];
+			buffer[1] = '\0';
+			add_token(&head, buffer);
+		}
+		else
+			buffer[buf_index++] = line[i];
+		i++;
+	}
+	if (buf_index > 0)
+	{
+		buffer[buf_index] = '\0';
+		add_token(&head, buffer);
+	}
+	return (head);
 }
 
 t_token	*add_token(t_token **head, char *str)
@@ -96,7 +108,7 @@ t_token	*add_token(t_token **head, char *str)
 	{
 		*head = new_token;
 	}
-	else 
+	else
 	{
 		temp = *head;
 		while (temp->next)
@@ -117,11 +129,11 @@ t_token_type	determine_token_type(char *str)
 	else if (ft_strcmp(str, ">") == 0)
 		return (REDIRECT_OUT);
 	else if (ft_strcmp(str, ">>") == 0)
-		return(APPEND);
-	else if (strcmp(str, "&&") == 0)
-        return (AND);
-	else if (strcmp(str, "||") == 0)
-        return (OR);
+		return (APPEND);
+	else if (ft_strcmp(str, "&&") == 0)
+		return (AND);
+	else if (ft_strcmp(str, "||") == 0)
+		return (OR);
 	else
 		return (COMMAND);
 }
