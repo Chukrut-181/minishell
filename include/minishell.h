2e6b1bff782cecf5348427cbecdc954250929e72
eandres <eandres@student.42urduliz.com>
2024-12-13 12:10:11 +0100

    update

--- include/minishell.h
+++ include/minishell.h
@@ -3,17 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eandres <eandres@student.42urdudilz.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:19:09 by eandres           #+#    #+#             */
-/*   Updated: 2024/12/13 11:45:24 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/12/13 11:51:00 by eandres          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// habria que incluir la libft y el pipex
 #include <readline/readline.h>
 #include <stdlib.h>
 #include <unistd.h>
