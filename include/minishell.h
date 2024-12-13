214a35ba69c1a91640c60f0072292a0d4eec5471
Ignacio Churruca Fernandez De Tejada <igchurru@c3r6s4.42urduliz.com>
2024-12-13 11:47:34 +0100

    Manually merged both header files into minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eandres <eandres@student.42urdudilz.com    +#+  +:+       +#+        */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:19:09 by eandres           #+#    #+#             */
-/*   Updated: 2024/12/13 11:29:17 by eandres          ###   ########.fr       */
+/*   Updated: 2024/12/13 11:45:24 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ typedef struct s_mini
 	char			*command;
 	char			**full_cmd;
 	char			*full_path;
-	char			**envp;
+	char			**env_vars;
 	char			**env_copy; 
 	int				is_builtin;
 	int				infile;
@@ -78,9 +78,29 @@ void	execute_external_command(t_mini *mini);
 //other
 void	free_args(char **args);
 
-//parse
-t_mini	*initialize_mini(char **env);
-char	*ft_strtrim_quotes(char const *s1, int squote, int dquote);
-int		count_quotes(char const *s1);
+//parse and command list creation
+t_mini	*ft_process_input(char *line, char **envp);
+char	**ft_split_prompt(char const *s, char c);
+char	*ft_expand_path(char *word);
+char	*ft_expand_variable(char *word, int index);
+char	*ft_strtrim_quotes(const char *s1, int squote, int dquote);
+int		ft_count_quotes(char const *s1);
+char	ft_get_quote_context(const char *s, int index);
+int		ft_get_var_name_len(const char *var_name);
+char	**ft_expand(char **temp);
+char	**ft_extract_operators(char **s, const char *operators);
+int		ft_arraylen(char **array);
+char	**ft_create_temp_array(char **array, int i, int j);
+char	**ft_concatenate_array(char **array, char **temp_array, int index);
+char	**ft_final_trim(char **array);
+t_mini	*ft_initialize_mini_node(char **envp);
+void	ft_get_full_envp(t_mini *node, char **envp);
+void	ft_check_redirections(t_mini *node, char **array);
+t_mini	*ft_create_structure(char **array, char **envp);
+void	ft_get_full_command(t_mini *node, char **array);
+int		ft_locate_pipe(char **array, int *index);
+void	ft_check_if_builtin(t_mini *node);
+void	ft_get_path(t_mini *node);
+void	ft_free_array(char **array);
 
 #endif
