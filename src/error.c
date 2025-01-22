9063f8ea38d246ce54c06088b73206de1d86f099
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2025-01-22 10:12:13 +0100

    Added definition for g_exit_status

--- src/error.c
+++ src/error.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eandres <eandres@student.42urdudilz.com    +#+  +:+       +#+        */
+/*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 13:07:30 by eandres           #+#    #+#             */
-/*   Updated: 2025/01/20 14:34:06 by eandres          ###   ########.fr       */
+/*   Updated: 2025/01/22 10:11:17 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int g_exit_status = 0;
 
 void	error(t_mini *mini, int sig, char *str)
 {
